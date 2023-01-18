5862b422fbc473f8cd8c30cfeed89b27f4b10508
Simon <simon.briaux2@gmail.com>
2023-01-18 15:47:11 +0100

    first commit

---
+++ Fonction.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Fonction</title>
+</head>
+<body>
+    <?php
+        Function tableaux(){
+    ?>
+        <table border=2>
+            <tr>
+                <td>preom</td>
+                <td>nom</td>
+            </tr>
+            <tr>
+                <td>simon</td>
+                <td>briaux</td>
+            </tr>
+        </table>
+    <?php
+        } 
+    ?>
+
+</body>
+</html>
