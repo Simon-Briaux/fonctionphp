330e22d493f128345471950fdee8ed4616d30cc3
Simon <simon.briaux2@gmail.com>
2023-01-27 16:24:13 +0100

    fin rdv

--- bdd2.php
+++ bdd2.php
@@ -4,99 +4,67 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>BASE DE DONNE</title>
+    
 </head>
 <body>
-<?php echo "<h1>coucou je suis pres et vous ?</h1>";
+<?php 
 
-if(isset($_POST["Valider"])){
-    echo "Idmedin = ".$_POST["idMedecin"]." id patien = ".$_POST["idPatient"]." date = ".$_POST["laDate"];
-    //COMME J'AI LES 2 ID
-    //je fait une requet pour faire un insert into consultation .... value ( idmedecin, idpatien, date)
-}
 
 try {
+   $ipserver="192.168.65.12";
+   $nomBase="medecin";
+   $loginPrivilege ="root";
+   $passPrivilege="root";
+   $GLOBALS["pdo"] = new PDO('mysql:host=' . $ipserver . ';dbname=' . $nomBase . '', $loginPrivilege, $passPrivilege); 
 
-    $ipserver ="192.168.65.12";
-    $nomBase = "Medecin";
-    $loginPrivilege ="SiteWeb";
-    $passPrivilege ="SiteWeb";
+?>
+    <?php
+   $requete= "select * from Medecin";
+   $requete1= "select * from Patient";
+   $resultat=$GLOBALS["pdo"]->query($requete);
+   $resultat1=$GLOBALS["pdo"]->query($requete1);
+ $tabMedecin = $resultat->fetchALL();
+ $tabPatient = $resultat1->fetchALL();
+ ?>
+ <form action=""method="post">
 
-    $GLOBALS["pdo"] = new PDO('mysql:host=' . $ipserver . ';dbname=' . $nomBase . '', $loginPrivilege, $passPrivilege);
-
-    $requete = "select * from Medecin";
-    $resultat = $GLOBALS["pdo"]->query($requete);
-    //resultat est du coup un objet de type PDOStatement
-    $tabMedecins = $resultat->fetchALL();
-
-    $requete = "select * from Patient";
-    $resultat = $GLOBALS["pdo"]->query($requete);
-    //resultat est du coup un objet de type PDOStatement
-    $tabPatients = $resultat->fetchALL();
+ <label for="idMedecin">Medecin:</label>
+ <select name="idMedecin">
+    <?php
+foreach ($tabMedecin as $Medecin){
     ?>
-    <form action="" method="post">
-        <select name="idMedecin">
-            <?php
-            foreach ($tabMedecins as $Medecin) {
-                ?>
-                <option value="<?=$Medecin["id"]?>"><?=$Medecin["nom"]." ".$Medecin["prenom"]?>s</option>
-                <?php
-            }
-            ?>
-        </select>
-        <select name="idPatient">
-            <?php
-            foreach ($tabPatients as $Patient) {
-                ?>
-                <option value="<?=$Patient["id"]?>"><?=$Patient["nom"]." ".$Patient["prenom"]?>s</option>
-                <?php
-            }
-            ?>
-        </select>
-        <input type="datetime-local" name="laDate">
-        <input type="submit" value="Saisir une consultation" name="Valider">
-    </form>
-    
+          <option value="<?=$Medecin["id"]?>"><?=$Medecin["nom"]." ".$Medecin["prenom"]?></option>
+          <?php
+  }?>
+ </select><br>
+<label for="idPatient">Patient:</label>
+ <select name="idPatient">
+<?php foreach ($tabPatient as $Patient){
+    ?>
+          <option value="<?=$Patient["id"]?>"><?=$Patient["nom"]." ".$Patient["prenom"]?></option>
+          <?php
+  }?>
+ </select><br>
+ <label for="time">TIME:</label>
+
+<input type="datetime-local" id="time" name="time"><br>
+<input type="submit" value="Send Request" />
+</form>
 <?php
+if (isset($_POST["idMedecin"])&&($_POST["idPatient"])) {
+    echo "id du medecin :". $_POST["idMedecin"];  
+    echo "<br>";
+    echo "id du patient :". $_POST["idPatient"];  
+    
+    $requete2 = "INSERT INTO `Consultation`( `Dateheure`, `idMedecin`, `idPatient`) VALUES ('" . $_POST['time'] . "','" . $_POST['idMedecin'] . "','" . $_POST['idPatient'] . "')";
+    $resultat2 = $GLOBALS["pdo"]->query($requete2);
+} 
+
+
 } catch (Exception  $error) {
     echo "error est : ".$error->getMessage();
 }
-
-
-/*
-
-RequetSql = "Select 
-Film.id
-,Film.titre,Film.resume,Film.lienImage, AVG(Note.note) as 'note'
-         FROM Film,Note,User
-        WHERE
-        
-Film.id
- = Note.idFilm
-        AND
-        Note.idUser = 
-User.id
-
-        AND
-        
-Film.id
- = '" . $id . "'  
-        Group By 
-Film.id
-;";
-
-        $resultat = $GLOBALS["pdo"]->query($RequetSql); //resultat sera de type pdoStatement
-        if ($resultat->rowCount() > 0) {
-            $tab = $resultat->fetch();
-            $this->id_ = $tab['id'];
-            $this->titre_ = $tab['titre'];
-            $this->resume_ = $tab['resume'];
-            $this->lienImage_ = $tab['lienImage'];
-            $this->MoyenneNote_ = $tab['note'];
-        }
-*/
-
-highlight_string( file_get_contents($_SERVER['SCRIPT_FILENAME']));
-?> 
+?>
 </body>
 </html>
